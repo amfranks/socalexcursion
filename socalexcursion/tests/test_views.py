5ec0b0a516a6be18b46de968b62f65b77d70d6c3
andrewmfranks <andrew.franks@bryanuniversity.edu>
2022-10-18 20:36:52 -0400

    make it better

---
+++ socalexcursion/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.test import Client
+
+class PageViewTests(TestCase):
+
+    c = Client()
+
+    def test_page_status_code(self):
+        response = self.c.get('/location/beachs')
+        self.assertEquals(response.status_code, 200)
